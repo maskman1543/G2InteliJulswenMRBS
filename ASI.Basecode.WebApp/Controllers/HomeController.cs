b2eccffdb1881a05ec0949d279f06c9b8f425ce7
Mask Man <maskman1543@gmail.com>
2024-11-20 20:50:50 +0800

    Camus - admin Dashboard with chart

--- ASI.Basecode.WebApp/Controllers/HomeController.cs
+++ ASI.Basecode.WebApp/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using ASI.Basecode.Services.Interfaces;
+using ASI.Basecode.Services.Services;
 using ASI.Basecode.WebApp.Mvc;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Linq;
 
 namespace ASI.Basecode.WebApp.Controllers
 {
@@ -14,6 +16,8 @@ namespace ASI.Basecode.WebApp.Controllers
     public class HomeController : ControllerBase<HomeController>
     {
         private readonly IRoomService _roomService;
+        private readonly IUserService _userService;
+        private readonly IBookingService _bookingService;
         /// <summary>
         /// Constructor
         /// </summary>
@@ -26,9 +30,13 @@ namespace ASI.Basecode.WebApp.Controllers
                              ILoggerFactory loggerFactory,
                              IConfiguration configuration,
                              IMapper mapper,
-                             IRoomService roomService) : base(httpContextAccessor, loggerFactory, configuration, mapper)
+                             IRoomService roomService,
+                             IUserService userService,
+                             IBookingService bookingService) : base(httpContextAccessor, loggerFactory, configuration, mapper)
         {
             _roomService = roomService;
+            _userService = userService;
+            _bookingService = bookingService;
         }
 
         /// <summary>
@@ -42,8 +50,16 @@ namespace ASI.Basecode.WebApp.Controllers
         [HttpGet]
         public IActionResult AdminDashboard()
         {
+            // Get the count of active rooms
+            var roomCount = _roomService.RetrieveActiveRooms().Count();
+            var userCount = _userService.RetrieveActiveNonAdminUsers().Count();
+            var BookingCount = _bookingService.RetrieveAllBookings().Count();
+            ViewBag.UserCount = userCount;
+            ViewBag.RoomCount = roomCount; // Passing the count to the view
+            ViewBag.BookingCount = BookingCount;
             return View();
         }
+
         [HttpGet]
         public IActionResult UserDashboard()
         {
